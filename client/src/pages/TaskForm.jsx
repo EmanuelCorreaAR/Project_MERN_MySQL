79a05e1827ff1f686055ca150bda10f2b6e862c5
EmanuelCorreaAR <correa.emanuel@outlook.com>
2023-02-12 17:08:37 -0300

    create_components

--- client/src/pages/TaskForm.jsx
+++ client/src/pages/TaskForm.jsx
@@ -1,20 +1,51 @@
 import { Formik, Form } from "formik";
+import { createTaskRequest } from "../api/tasks.api";
 
 function TaskForm() {
   return (
     <div>
-      <Formik>
-        <Form>
-          <label>title</label>
-          <input type="text" name="title" placeholder="Write a title" />
-          <label>description</label>
-          <textarea
-            name="description"
-            rows="5"
-            placeholder="Write a description"
-          ></textarea>
-          <button>Save</button>
-        </Form>
+      <Formik
+        initialValues={{
+          //definimos estados iniciales en Formik, no tengo que usar useState y mi propio manejador de estados.
+          title: "",
+          description: "",
+        }}
+        onSubmit={async (values, actions) => {
+          console.log(values);
+          try {
+            const response = await createTaskRequest(values);
+            console.log(response);
+            actions.resetForm();
+          } catch (error) {
+            console.error(error);
+          }
+        }}
+      >
+        {(
+          { handleChange, handleSubmit, values, isSubmitting } //metemos el formulario dentro de una funcion jsx
+        ) => (
+          <Form onSubmit={handleSubmit}>
+            <label>title</label>
+            <input
+              type="text"
+              name="title"
+              placeholder="Write a title"
+              onChange={handleChange}
+              value={values.title}
+            />
+            <label>description</label>
+            <textarea
+              name="description"
+              rows="5"
+              placeholder="Write a description"
+              onChange={handleChange}
+              value={values.description}
+            ></textarea>
+            <button type="submit" disabled={isSubmitting}>
+              {isSubmitting ? "Saving..." : "Save"}
+            </button>
+          </Form>
+        )}
       </Formik>
     </div>
   );
--- server/index.js
+++ server/index.js
@@ -1,9 +1,14 @@
 import express from 'express';
 import { PORT } from './config.js';
+import cors from 'cors';
 import indexRoutes from './routes/index.routes.js'
 import taskRoutes from './routes/task.routes.js'
 
 const app = express();
+
+app.use(cors({
+    origin:"http://localhost:5173"
+}));
 
 app.use(express.json());
 
