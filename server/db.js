3c3728867ac4bb391d0a2d5cd0f223ee91095674
EmanuelCorreaAR <correa.emanuel@outlook.com>
2023-02-13 03:17:58 -0300

    production

--- server/db.js
+++ server/db.js
@@ -1,16 +1,16 @@
 import { createPool } from 'mysql2/promise';
 import {
-    DB_HOST,
-    DB_USER,
-    DB_PASSWORD,
-    DB_NAME,
-    DB_PORT
+    MYSQLHOST,
+    MYSQLUSER,
+    MYSQLPASSWORD,
+    MYSQLDATABASE,
+    MYSQLPORT
 } from './config.js';
 
 export const pool = createPool({
-    host: DB_HOST,
-    port: DB_PORT,
-    user: DB_USER,
-    password: DB_PASSWORD,
-    database: DB_NAME
+    host: MYSQLHOST,
+    port: MYSQLPORT,
+    user: MYSQLUSER,
+    password: MYSQLPASSWORD,
+    database: MYSQLDATABASE
 });
