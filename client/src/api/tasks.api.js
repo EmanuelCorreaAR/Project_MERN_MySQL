7979f750018ddc1febb6fcc35487523c2bcc7dbd
EmanuelCorreaAR <correa.emanuel@outlook.com>
2023-02-13 15:35:43 -0300

    production state

--- client/src/api/tasks.api.js
+++ client/src/api/tasks.api.js
@@ -2,26 +2,26 @@ import axios from 'axios'
 
 //Obtener
 export const getTasksRequest = async () =>
-    await axios.get('http://localhost:4000/tasks');
+    await axios.get('projectmernmysql-production.up.railway.app/');
 
 //Crear
 export const createTaskRequest = async (task) =>
-    await axios.post('http://localhost:4000/tasks', task);
+    await axios.post('projectmernmysql-production.up.railway.app/', task);
 
 //Borrar
 export const deleteTaskRequest = async (id) =>
-    await axios.delete(`http://localhost:4000/tasks/${id}`);
+    await axios.delete(`projectmernmysql-production.up.railway.app/${id}`);
 
 //obtiene una tarea
 export const getTaskRequest = async (id) =>
-    await axios.get(`http://localhost:4000/tasks/${id}`);
+    await axios.get(`projectmernmysql-production.up.railway.app/${id}`);
 
 //actualiza una tarea
 export const updateTaskRequest = async (id, newFields) =>
-    await axios.put(`http://localhost:4000/tasks/${id}`, newFields);
+    await axios.put(`projectmernmysql-production.up.railway.app/${id}`, newFields);
 
 //para el toogle en true o false
 export const toggleTaskDoneRequest = async (id, done) =>
-    await axios.put(`http://localhost:4000/tasks/${id}`, {
+    await axios.put(`projectmernmysql-production.up.railway.app/${id}`, {
         done,
     });
